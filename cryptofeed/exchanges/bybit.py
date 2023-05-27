8fad2b98feb8d8dec479a38275cde7dbbff0a142
gautier pialat <gautier.pialat@notonlygeek.com>
2023-05-27 03:16:35 +0200

    Update bybit.py

--- cryptofeed/exchanges/bybit.py
+++ cryptofeed/exchanges/bybit.py
@@ -162,6 +162,7 @@ class Bybit(Feed):
 
         if "success" in msg:
             if msg['success']:
+                LOG.error("msg is", msg)
                 if msg['request']['op'] == 'auth':
                     LOG.debug("%s: Authenticated successful", conn.uuid)
                 elif msg['request']['op'] == 'subscribe':
